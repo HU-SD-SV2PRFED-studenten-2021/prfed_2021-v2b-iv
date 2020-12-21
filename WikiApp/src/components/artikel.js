6d5f1b5b8a9e7949a4456e8a906658f5fe7cdd46
Ibrahim Aldarra <ibrahim.aldarra@student.hu.nl>
2020-12-21 06:55:39 +0100

    verbeterd artikel

--- WikiApp/src/components/artikel.js
+++ WikiApp/src/components/artikel.js
@@ -1,18 +1,33 @@
 import { LitElement, html, css } from 'https://cdn.skypack.dev/lit-element@2.3.1';
-
+import { classMap } from '/lit-html/directives/class-map.js';
 
 class WikiArtikel extends LitElement {
     static get properties() {
         return {
             id: { type: String, attribute: 'id', reflect: true },
+            datum: { type: Date, reflect: true },
             titel: { type: String, reflect: true },
-            tekst: { type: String, reflect: true }
+            tekst: { type: String, reflect: true },
+            tags: { type: Array, reflect: true}, 
+            collapsed: { type: Boolean, reflect: true }
         }
+    }
+
+
+    constructor() {
+        super();
+
+        this.datum = Date.now();
+        this.title = "";
+        this.tekst = "";
+        this.tags = [];
+
+        this.collapsed = true;
     }
 
     static get styles() {
         return css`
-            .artikel-cont {
+            :host {
                 border: 0.1em solid rgb(46, 42, 42);
                 margin-top: 3em;
                 padding: 1em;
@@ -20,20 +35,33 @@ class WikiArtikel extends LitElement {
         `;
     }
 
+    handelCollapseArtikel() {
+        this.collapsed = !this.collapsed;
+    }
+
 
     render() {
-        return html`
-            <div class="artikel-cont">
-            
-            <h1>
-                <a href="artikelen/${this.id}">
-                    <slot name="titel">${this.titel}</slot>
-                </a>
-            </h1>
-                
-                <slot name="tekst">${this.tekst}</slot>
+      return html`
+        <div class="artikel-header">
+            <h1 class="titel">${this.titel}</h1>
+            <p class="datum">${this.datum}</p>
+        </div>
+        <div class="artikel-main">
+            <p class="tekst">${this.tekst}</p>
+        </div>
+        <div class="artikel-footer">
+            <div class="tags-cont">
+                <ul class="tags-lst">
+                    ${this.tags.map((tag) => html`
+                        <li class="tag">${tag}</li>
+                    `)}
+                </ul>
             </div>
-        `;
+            <button @click="${this.handelCollapseArtikel}"
+                    class="collapse-btn ${classMap({ collapsed: this.collapsed })}">Lees meer</button>
+        </div>
+        
+        `
     }
 }
 
