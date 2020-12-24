fb48929925739c3991159536abe5562e17182fb4
Ibrahim Aldarra <ibrahim.aldarra@student.hu.nl>
2020-12-24 20:15:01 +0100

    indentation fix

--- WikiApp/src/components/radio-button.js
+++ WikiApp/src/components/radio-button.js
@@ -2,60 +2,63 @@ import {LitElement, html, css} from 'https://cdn.skypack.dev/lit-element@2.3.1'
 
 const styles = css`
     label {
-  display: -webkit-box;
-  display: flex;
-  cursor: pointer;
-  font-weight: 500;
-  position: relative;
-  overflow: hidden;
-  margin-bottom: 0.375em;  
+        display: -webkit-box;
+        display: flex;
+        cursor: pointer;
+        font-weight: 500;
+        position: relative;
+        overflow: hidden;
+        margin-bottom: 0.375em;  
     }
     
-label input {
-            position: absolute;
-            left: -9999px;
-            }
+    label input {
+        position: absolute;
+        left: -9999px;
+    }
             
-label input:checked + span {
-                              background-color: #d6d6e5;
-                            }
+    label input:checked + span {
+        background-color: #d6d6e5;
+    }
                             
-label input:checked + span:before {
-                                      box-shadow: inset 0 0 0 0.4375em #00005c;
-                                    }
-label span {
-              display: -webkit-box;
-              display: flex;
-              -webkit-box-align: center;
-                      align-items: center;
-              padding: 0.375em 0.75em 0.375em 0.375em;
-              border-radius: 99em;
-              -webkit-transition: 0.25s ease;
-              transition: 0.25s ease;
-            }
-label span:hover {
-                  background-color: #d6d6e5;
-                }
-label span:before {
-                  display: -webkit-box;
-                  display: flex;
-                  flex-shrink: 0;
-                  content: "";
-                  background-color: #fff;
-                  width: 1.5em;
-                  height: 1.5em;
-                  border-radius: 50%;
-                  margin-right: 0.375em;
-                  -webkit-transition: 0.25s ease;
-                  transition: 0.25s ease;
-                  box-shadow: inset 0 0 0 0.125em #00005c;
-                }
+    label input:checked + span:before {
+        box-shadow: inset 0 0 0 0.4375em #00005c;
+    }
+    
+    label span {
+        display: -webkit-box;
+        display: flex;
+        -webkit-box-align: center;
+        align-items: center;
+        padding: 0.375em 0.75em 0.375em 0.375em;
+        border-radius: 99em;
+        -webkit-transition: 0.25s ease;
+        transition: 0.25s ease;
+    }
+
+    label span:hover {
+        background-color: #d6d6e5;
+    }
+
+    label span:before {
+        display: -webkit-box;
+        display: flex;
+        flex-shrink: 0;
+        content: "";
+        background-color: #fff;
+        width: 1.5em;
+        height: 1.5em;
+        border-radius: 50%;
+        margin-right: 0.375em;
+        -webkit-transition: 0.25s ease;
+        transition: 0.25s ease;
+        box-shadow: inset 0 0 0 0.125em #00005c;
+    }
                 
-.radio-button-container{
-                        display: flex;
-                        flex-flow: row wrap;
-                        justify-content: space-around;              
-                        }
+    .radio-button-container{
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-around;              
+    }
 
 `
 
