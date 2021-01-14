ea1d17e9bcbd746ad547c9758be6b329ebf62bdc
fare-alkhadrawy <fars.alkhadrawy@student.hu.nl>
2021-01-14 16:41:02 +0100

    categorie pagina aangepast en toegevoegd

--- WikiApp/src/components/category-redirect.js
+++ WikiApp/src/components/category-redirect.js
@@ -1,10 +1,23 @@
 import { LitElement, html, css } from 'https://cdn.skypack.dev/lit-element@2.3.1';
 
 const styles = css`
-a{
-    color: #303030;
-    text-decoration: none;
-}
+
+  a {
+        padding: 5px;
+        text-align: center;
+        margin: 0;
+        border-top: 1px solid rgb(207 207 207 / 84%);
+    }
+
+  a:hover {
+        background-color: rgb(223 223 223);
+    }
+
+
+  a {
+        text-decoration: none;
+        color: black;
+    }
 `
 
 class categoryRedirect extends LitElement{
@@ -28,6 +41,7 @@ class categoryRedirect extends LitElement{
     render(){
         return html`
         <a href="${this.link}">${this.category}</a>
+        
         `
     }
 
