f686e13e323810a6cabab0557120c496fc04002d
fare-alkhadrawy <fars.alkhadrawy@student.hu.nl>
2021-01-14 13:40:41 +0100

    RB

--- WikiApp/src/components/radio-button.js
+++ WikiApp/src/components/radio-button.js
@@ -1,50 +1,7 @@
 import {LitElement, html, css} from 'https://cdn.skypack.dev/lit-element@2.3.1'
 
 const styles = css`
-    label input {
-        display: none;
-    }
-            
-    label input:checked + span {
-        background-color: #d6d6e5;
-    }
-                            
-    label input:checked + span:before {
-        box-shadow: inset 0 0 0 0.4375em #00005c;
-    }
-    
-    label span {
-        display: -webkit-box;
-        display: flex;
-        -webkit-box-align: center;
-        align-items: center;
-        padding: 0.375em 0.75em 0.375em 0.375em;
-        border-radius: 30px;
-        transition: 0.25s ease;
-    }
 
-    label span:hover {
-        background-color: #d6d6e5;
-    }
-
-    label span:before {
-        display: flex;
-        flex-shrink: 0;
-        content: "";
-        background-color: #fff;
-        width: 1.5em;
-        height: 1.5em;
-        border-radius: 100%;
-        margin-right: 0.375em;
-        transition: 0.25s ease;
-        box-shadow: inset 0 0 0 0.125em #00005c;
-    }
-                
-    .radio-button-container{
-        display: flex;
-        flex-flow: row wrap;
-        justify-content: space-around;              
-    }
 
 `
 
@@ -107,10 +64,10 @@ class radioButton extends LitElement {
             
             ${Object.values(VisibilityFilter).map(filter =>
                  html`
-                    <label>
+                    <label> ${filter}
                         <input type="radio" name="filter" value="${filter}"  checked  @change="${this.emitChange()}" />
-                        <span>${filter}</span>
-                    </label>`
+                    </label>
+                   `
         )}
         </div>
         `;
