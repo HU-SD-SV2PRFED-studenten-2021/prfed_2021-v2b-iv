94ceceea63a2859e525d74e0c53904d77745bd0e
Ibrahim Aldarra <ibrahim.aldarra@student.hu.nl>
2021-01-18 16:00:24 +0100

    article toevoegen form stylized

--- WikiApp/src/components/article-toevoegen.js
+++ WikiApp/src/components/article-toevoegen.js
@@ -1,75 +1,81 @@
 import { LitElement, html, css } from 'https://cdn.skypack.dev/lit-element@2.3.1';
 import getData from "../utils/get-data.js";
 
-const styles = css`
-        .toevoegen {
-        padding: 2em;
-        height: 100%;
-        }
-        fieldset{
-            background: #f9f9f9;
-            box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-            padding: 1em;
-            height: 600px;
-            border-radius: 10px;
-        
-        }
-        label{
-            display: block;
-            margin: 0px 0px 40px 40px;
-        }
-        label > span{
-            width: 100px;
-            font-weight: bold;
-            float: left;
-            padding-top: 8px;
-            padding-right: 5px;
-        }
-        
-        input.input-field, .select-field{
-            width: 60%;
-        }
-        input.input-field,
-        .textarea-field,
-                .select-field{
-                    box-sizing: border-box;
-                    -webkit-box-sizing: border-box;
-                    -moz-box-sizing: border-box;
-                    border: 1px solid #C2C2C2;
-                    box-shadow: 1px 1px 4px #EBEBEB;
-                    -moz-box-shadow: 1px 1px 4px #EBEBEB;
-                    -webkit-box-shadow: 1px 1px 4px rgb(29 113 185);
-                    border-radius: 3px;
-                    -webkit-border-radius: 3px;
-                    -moz-border-radius: 3px;
-                    padding: 7px;
-                    outline: none;
-                }
-        .input-field:focus,
-        .textarea-field:focus,
-        .select-field:focus{
-            border: 1px solid rgb(29 113 185);
-        }
-        .textarea-field{
-            height:300px;
-            width: 100%;
-        }
-`
 
 
 class articleToevoegen extends LitElement{
 
     static get styles(){
-        return styles;
+        return css`
+
+        h2 {
+            padding-left: 1.3em;
+        }
+    
+    
+        .form-cont {
+            padding: 2em;
+            background: #fbfbfb;
+            margin: 1em 2em;
+            border: 1px solid #efefef;
+            border-radius: 15px;
+            box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+        }
+
+
+        .form-row-cont {
+            display: flex;
+            flex-direction: column;
+            margin-bottom: 1.5em;
+        }
+    
+        fieldset {
+            border: none;
+        }
+
+        legend {
+            display: none;
+        }
+
+        .form-input {
+            font-size: .9em;
+            box-sizing: border-box;
+            border: 1px solid #C2C2C2;
+            border-radius: 3px;
+            padding: .7em;
+            margin-left: .5em;
+            outline: none;
+        }
+
+
+
+        .form-input:focus {
+            border: 1px solid #000000;
+        }
+
+        #select-field {
+            margin-top: .5em;
+            max-width: 280px;
+        }
+
+        #tekstarea-label {
+            display: none;
+        }
+        textarea {
+            font-family: sans-serif;
+            resize: vertical;
+            height: 100%;
+            min-height: 10em;
+        }
+    `    
     }
 
-    static get properties(){
-        return{
-            articleData :{ type: Array},
-            titel:{type: String},
-            category:{type:String},
-            text:{type:String}
-
+    static get properties() {
+        return {
+            articleData: { type: Array },
+            titel: { type: String },
+            category: {type:String },
+            text: { type:String }
         }
     }
 
@@ -80,15 +86,17 @@ class articleToevoegen extends LitElement{
         this.category='';
     }
 
-    titelInput(e){
+    titelInput(e) {
         this.titel = e.target.value;
         console.log(this.titel)
     }
-    categoryInput(e){
+
+    categoryInput(e) {
         this.category = e.target.value;
         console.log(this.category)
     }
-    textInput(e){
+    
+    textInput(e) {
         this.text = e.target.value;
         console.log(this.titel)
     }
@@ -97,35 +105,31 @@ class articleToevoegen extends LitElement{
 
     render(){
         return html`
-          <div class="toevoegen">
-            <h2>Nieuwe Artikel Toevoegen</h2>
-           
-                <fieldset>
-                <legend>Artikel</legend>
-                    <div class="row">
-                        <label for="field1">
-                            <span>Titel </span>
-                            <input @keyup="${this.titelInput}" type="text" class="input-field" name="field1" />
-                        </label>
-                    </div>
-                    <div class="row">
-                        <label><span> Select Categorie</span>
-                            <select class="select-field" @change="${this.categoryInput}">
-                            <option selected hidden >Kies een category</option>
-                                ${this.articleData.map(artikel =>html`
-                                <option value="${artikel}">${artikel}</option>`)}
-                            </select>
-                        </label>
-                        
-                    </div>
-                    <div class="row">
-                        <label><span>Artikel Tekst</span>
-                        <textarea class="textarea-field" @keyup="${this.textInput}"></textarea>
-                        </label>
-                    </div>
-                    <toevoegen-bevestiging  .titel="${this.titel}" .category="${this.category}" .text="${this.text}"></toevoegen-bevestiging>
-
-                </fieldset>
+        <h2>Nieuwe artikel toevoegen</h2>
+        <div class="form-cont">
+            <form>
+                <legend>Nieuwe artikel toevoegen</legend>
+                <div class="form-row-cont">
+                    <label for="titel-input">Titel</label>
+                    <input @keyup="${this.titelInput}" type="text" id="titel-input" class="form-input" name="titel-input" />
+                </div>
+                <div class="form-row-cont">
+                    <label for="select-field">Artikel Categorie</label>
+                    <select id="select-field" class="form-input" @change="${this.categoryInput}">
+                    <option selected hidden >Kies een category</option>
+                        ${this.articleData.map(artikel => html`
+                            <option value="${artikel}">
+                                ${artikel}
+                            </option>
+                        `)}
+                    </select>
+                </div>
+                <div class="form-row-cont">
+                    <label for="tekst-input" id="tekstarea-label">Artikeltekst</label>
+                    <textarea id="tekst-input" class="form-input" @keyup="${this.textInput}"></textarea>
+                </div>                    
+                <toevoegen-bevestiging  .titel="${this.titel}" .category="${this.category}" .text="${this.text}"></toevoegen-bevestiging>
+            </form>
             
           </div>
         `
